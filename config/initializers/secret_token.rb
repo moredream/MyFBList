b91983b8617375bc2bc47500a7a519ded0301183
hyunukJung <moredream7@gmail.com>
2011-12-12 18:39:17 +0900

    v0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfblist::Application.config.secret_token = '77515898e295f2451f5a3cfba9f7b6ce691da4dde07226a7badc27ae8ce866220ca9d14df73171eb0e131d520490c08ad5ad6dce63b619fcbe775bbef15da489'
