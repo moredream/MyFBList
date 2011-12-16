574acd7311a26a4fc714be6a3e5ed8b58333bed0
hyunukJung <moredream7@gmail.com>
2011-12-16 12:59:32 +0900

    v.51

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,38 +11,83 @@
 $(document).ready(function () {
 
     $('input[name$="validation"]').click(function () {
+        validate_facebook_id();
 
-        var get_facebook_info = "";
+    });
+
+    $("#new_post").submit(function(){
+
+        var rt1 = validate_field("#post_name", "#msg_name");
+        var rt2 = validate_field("#post_url", "#msg_url");
+        var rt3 = validate_field("#post_description", "#msg_description");
+
+        if(rt1 && rt2 && rt3) {
+            return true;
+        }else{
+            return false;
+        }
+
+
+    });
+
+    $("#post_name").blur(function(){
+        validate_field("#post_name", "#msg_name");
+    });
+    $("#post_url").blur(function(){
+        validate_field("#post_url", "#msg_url");
+    });
+    $("#post_description").blur(function(){
+        validate_field("#post_description", "#msg_description");
+    });
+
+    function validate_facebook_id(){
+        $("#msg_f").text('');
+
+         var get_facebook_info = "";
         // check validation facebook_id field
         var facebook_id_s = $("#post_facebook_id").val();
-        if(facebook_id_s.length > 0)             {
+        if(facebook_id_s.length > 0){
+
+
           get_facebook_info ="https://graph.facebook.com/"+    facebook_id_s;
+
           $.getJSON(get_facebook_info, function(data) {
-
           $("#post_facebook_id").val(data.id);
-
-          prepare_input("#post_name");
-          prepare_input("#post_url");
-          prepare_input("#post_description");
-          prepare_input("#post_count");
-
           $("#post_name").val(data.name);
           $("#post_url").val(data.link);
           $("#post_description").val(data.description);
-
-        //  $("#post_count").val('0');
-
-
-        });
+          })
+          .success(function() {readOnly_input("#post_facebook_id");})
+          .error(function() {$("#msg_f").text('Invalid facebook ID or wrong url');});
         }else{
-            alert("type your facebook id or facebook name");
+            $("#msg_f").text("please, enter your facebook id or facebook name");
         }
 
-    });
+    }
+
+    function validate_field(id, msg){
+        var rval = false;
+        if($(id).val().length == 0){
+            $(msg).text('This field is required.');
+            rval = false;
+        } else{
+            $(msg).text('');
+            rval = true;
+        }
+
+        return rval;
+    }
 
     function prepare_input(id){
           $(id).attr("class", "textc");
           $(id).removeAttr("disabled");
+
     }
+    function readOnly_input(id){
+        $(id).attr("readonly", "true");
+        $(id).attr("class", "textc-fleeze");
+
+    }
+
 
 });
